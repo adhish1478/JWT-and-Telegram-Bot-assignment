61b9a48b93fc45457b867954368a4de503fa7304
adhish1478 <adhish.aravind7@gmail.com>
2025-06-12 15:42:54 +0530

    registration feat added- serializer validates the data. api endpoints routed at urls, with functions like register, token generation and token refresh

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,13 @@
+# user/urls.py
+
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView
 
 urlpatterns=[
-    
+# URL patterns for user authentication- token generation and refresh
+    path('token/',TokenObtainPairView.as_view(), name= 'token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+# URL patterns for user registration
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,16 @@
+# user/views.py
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
+from rest_framework import status
 # Create your views here.
+
+# User registration view to handle user creation
+class RegisterView(APIView):
+    def post(self, request):
+        serializer= RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user= serializer.save()
+            return Response({'message': 'User registered successfully!'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
