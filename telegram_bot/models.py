ccdcd3bcbfa93f62f88f777a90f288a50ac38dca
adhish1478 <adhish.aravind7@gmail.com>
2025-06-13 17:25:16 +0530

    model made for telegram bot username retrieval, views made to get info upon /start request, and urls connected

--- telegram_bot/models.py
+++ telegram_bot/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class TelegramUser(models.Model):
+    telegram_id = models.CharField(max_length=100, unique=True)
+    username = models.CharField(max_length=100, blank=True, null=True)
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.username or self.telegram_id
--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+# telegram_bot/views.py
 
-# Create your views here.
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from .models import TelegramUser
+
+@csrf_exempt
+def telegram_webhook(request):
+    if request.method == "POST":
+        print("Webhook hit ✅")
+        data = json.loads(request.body)
+
+        message = data.get("message", {})
+        if message.get("text") == "/start":
+            user = message.get("from", {})
+            telegram_id = user.get("id")
+            username = user.get("username")
+            first_name = user.get("first_name")
+            last_name = user.get("last_name")
+
+            TelegramUser.objects.update_or_create(
+                telegram_id=telegram_id,
+                defaults={
+                    "username": username,
+                    "first_name": first_name,
+                    "last_name": last_name,
+                }
+            )
+
+        return JsonResponse({"ok": True})
+    return JsonResponse({"error": "Invalid request"}, status=400)
+    
