868d5043a56054786afcdbda72dcae7f96b634d7
adhish1478 <adhish.aravind7@gmail.com>
2025-06-13 21:26:44 +0530

    admin panel for telegram bot

--- telegram_bot/admin.py
+++ telegram_bot/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import TelegramUser
+
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'telegram_id', 'first_name', 'last_name')
+    search_fields = ('username', 'telegram_id', 'first_name')
