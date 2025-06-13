8d5ef0c42dba1aef1be06c326304ec117b018f7b
adhish1478 <adhish.aravind7@gmail.com>
2025-06-13 21:27:32 +0530

    admin panel for news

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import News
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    list_filter = ('author', 'created_at')
+    search_fields = ('title', 'content', 'author__username')
